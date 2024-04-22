9b8e154aa3a3ef37cd9ae347e346460eb5d20f8a
LiamSheldonn <liamsheldon05@gmail.com>
2024-04-22 11:15:24 -0400

    t1

--- symbolic_algebra/lab.py
+++ symbolic_algebra/lab.py
@@ -43,7 +43,50 @@ class Num(Symbol):
     def __repr__(self):
         return f"Num({self.n})"
 
+class BinOp():
+    def __init__(self,left,right):
+        true_vals = []
+        for val in (left,right):
+            if isinstance(val, Var) or isinstance(val, Num):
+                true_vals.append(val)
+            elif isinstance(val,str):
+                true_vals.append(Var(val))
+            elif isinstance(val, int) or isinstance(val, float):
+                true_vals.append(Num(val))
+            else:
+                raise TypeError
+        self.left, self.right = true_vals
+    
+    def represent(self, operator):
+        return f"{operator}({repr(self.left)}, {repr(self.right)})"
+    def string(self, operator):
+        return f"{self.left} {operator} {self.right}"
+
+class Add(BinOp):
+    def __repr__(self):
+        return self.represent("Add")
+    def __str__(self):
+        return self.string('+')
+
+class Sub(BinOp):
+    def __repr__(self):
+        return self.represent("Sub")
+    def __str__(self):
+        return self.string('-')
+
+class Mul(BinOp):
+    def __repr__(self):
+        return self.represent("Mul")
+    def __str__(self):
+        return self.string('*')
+class Div(BinOp):
+    def __repr__(self):
+        return self.represent("Div")
+    def __str__(self):
+        return self.string('/')
+
 
 if __name__ == "__main__":
     doctest.testmod()
-    print('Test')
+    z = Add(Var('x'), Sub(Var('y'), Num(2)))
+    print(repr(z),z)
